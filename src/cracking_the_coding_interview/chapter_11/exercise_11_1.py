c5f89e71e594224e87b27bf5fda974f58cbbd806
Matthias Bachfischer <bachfischer.matthias@googlemail.com>
2021-11-27 20:29:02 +0200

    add solution for exercise 11.1 from "cracking the coding interview"

---
+++ src/cracking_the_coding_interview/chapter_11/exercise_11_1.py
@@ -0,0 +1,13 @@
+"""
+Mistake:
+Find the mistake(s) in the following code:
+"""
+"""
+unsigned int i;
+for(i = 100; i >= 0; --i):
+    printf("%d\n", i);
+    
+Answer: uint is always greater than or equal to 0, so for loop is always true
+        May want to change %d in printf statement to %u 
+"""
+
