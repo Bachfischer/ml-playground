0fd557535dbf5e01a66043a56676540b2629b134
Matthias Bachfischer <matthias.bachfischer@ibm.com>
2022-10-10 16:58:52 +0200

    add code for experiments with PyTorch Lightning

---
+++ src/pytorch-lightning/lit_auto_encoder.py
@@ -0,0 +1,59 @@
+import os
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
+import pytorch_lightning as pl
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.l1 = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
+
+    def forward(self, x):
+        return self.l1(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.l1 = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
+
+    def forward(self, x):
+        return self.l1(x)
+
+# define the LightningModule
+class LitAutoEncoder(pl.LightningModule):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop.
+        # it is independent of forward
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = nn.functional.mse_loss(x_hat, x)
+        # Logging to TensorBoard by default
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+data_dir = os.path.join(os.getcwd(), "./data")
+dataset = MNIST(data_dir, download=True, transform=transforms.ToTensor())
+train_loader = DataLoader(dataset)
+
+# model
+autoencoder = LitAutoEncoder(Encoder(), Decoder())
+
+# train model
+trainer = pl.Trainer()
+trainer.fit(model=autoencoder, train_dataloaders=train_loader)
